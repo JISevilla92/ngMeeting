8e641767a81318e495fa75e74e23e38eedccc52c
Mishu Agopian <mishuagopian@gmail.com>
2015-06-24 12:21:24 -0300

    i18n

--- src/app/app.translations.js
+++ src/app/app.translations.js
@@ -3,14 +3,20 @@ app.config([
   function($translateProvider) {
 
   $translateProvider.translations('es', {
-    DAYS_OF_WEEK: {
-      SUNDAY: 'Domingo',
-      MONDAY: 'Lunes',
-      TUESDAY: 'Martes',
-      WEDNESDAY: 'Miércoles',
-      THURSDAY: 'Jueves',
-      FRIDAY: 'Viernes',
-      SATURDAY: 'Sabado'
+    COMPONENT_1_PHRASE: 'Esta es la componente 1',
+    SIGN_UP_FORM: {
+      CONFIRM_PASSWORD: 'Confirmar contraseña',
+      EMAIL: 'Email',
+      ERROR: {
+        EMAIL_LENGTH: 'Longitud inválida: debería ser de entre 8 y 64 caracteres.',
+        NAME_LENGTH: 'Longitud inválida: debería ser de entre 2 y 64 caracteres.',
+        PASSWORD_LENGTH: 'Longitud inválida: debería ser de entre 8 y 32 caracteres.'
+      },
+      FIRSTNAME: 'Nombre',
+      LASTNAME: 'Apellido',
+      PASSWORD: 'Contraseña',
+      SIGN_UP: 'Registrarme',
+      USERNAME: 'Nombre de usuario'
     }
   });
   $translateProvider.preferredLanguage('es');
--- src/app/components/component1/component1Controller.js
+++ src/app/components/component1/component1Controller.js
@@ -1,4 +1,3 @@
 angular.module('app').controller('Component1Controller', [
 function() {
-  this.component1Phrase = 'This is component 1';
 }]);
